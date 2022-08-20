2f4620d3c41190b9e48b5a1feaaff7115d2f80e4
Raman <romadmukhouski@gmail.com>
2022-08-20 17:12:12 +0200

    First API - 02

---
+++ src/main/java/co/develhope/springdevelhope/controllers/FactorialController.java
@@ -0,0 +1,21 @@
+package co.develhope.springdevelhope.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigInteger;
+
+@RestController
+public class FactorialController {
+
+    @GetMapping(value = "/controller/{n}")
+    public BigInteger getFactorial(@PathVariable int n){
+        BigInteger result = BigInteger.ONE;
+        for(int i = 2; i <=n ; i++) {
+            result = result.multiply(BigInteger.valueOf(i));
+        }
+        return result;
+    }
+}
