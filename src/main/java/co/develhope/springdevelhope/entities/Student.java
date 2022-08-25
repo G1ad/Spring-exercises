3583c593aeda1008018e236c9c1b204d435ba241
Raman <romadmukhouski@gmail.com>
2022-08-25 15:18:20 +0200

    Database - Mock Database

---
+++ src/main/java/co/develhope/springdevelhope/entities/Student.java
@@ -0,0 +1,51 @@
+package co.develhope.springdevelhope.entities;
+
+import javax.persistence.*;
+
+@Table
+@Entity
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String firstName;
+    private String lastName;
+    @Column(unique = true)
+    private String email;
+
+    public Student() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
